4ddad028e765bc7b109af356ea37451c854b8900
Bazil Baranov <baranov.bazil@gmail.com>
2021-03-27 20:55:24 +0300

    Пробую декоратор cache в рекурсии числе фибонначи

--- first.py
+++ first.py
@@ -1,1 +1,16 @@
-print('My first git-repo')
+def fib(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def main():
+    for i in range(int(input())):
+        print(fib(i))
+
+
+if __name__ == '__main__':
+    main()
